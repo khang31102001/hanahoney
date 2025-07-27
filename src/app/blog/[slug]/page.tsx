d6c934ea10b207bfa0d9fc25d0d818eabf562d87
khang31102001 <leminhkhang.ltp@gmail.com>
2025-07-27 22:28:45 +0700

    update fix bug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,19 +1,27 @@
 import BlogDetail from "@/components/Blog/blogDetail";
-import {  blog } from "@/data/blog";
+// import {  blog } from "@/data/blog";
 
-// interface BlogProps {
-//   params: {
-//     slug: string
-//   }
-// }
+interface BlogProps {
+  params: {
+    slug: string
+  }
+}
 export async function generateStaticParams() {
-  return blog.map((item)=>({
+  try{
+    const blog = await import('@/data/blog').then(mod => mod.blog);
+    return blog.map((item)=>({
     slug: item.slug
   }));
+  }catch (error) {
+    console.error("Error generating static params:", error);
+    // Handle the error appropriately, e.g., return an empty array or rethrow
+    return [];
+  }
+  
 }
-export default async function BlogDetailPage({ params }: { params: { slug: string } }) {
-    const { slug } = params;
-    const findBlog = blog.find(item => item.slug === slug);
+export default async function BlogDetailPage({ params }:BlogProps ) {
+    const blog = await import('@/data/blog').then(mod => mod.blog);
+    const findBlog = blog.find(item => item.slug === params.slug);
     if (!findBlog) return null;
     return(
         <main>
